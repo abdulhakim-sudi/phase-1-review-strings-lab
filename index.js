76875b6f4d59e8cd006f874c5db219d0cdc55390
ABDULHAKIM <abdulhakimsudi27@gmail.com>
2025-03-24 12:12:59 +0300

    Implemented user greeting features

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Step 1: Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Step 2: Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Step 3: Create excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Step 4: Create shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
